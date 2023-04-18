c7f53e6ee3098ce4992a468be69b5efa24ffaf96
Griffith <shuqike.research@gmail.com>
2023-04-18 23:43:15 +0800

    Update pt_vit.py

--- src/models/pt_vit.py
+++ src/models/pt_vit.py
@@ -3,5 +3,13 @@ from transformers import ViTImageProcessor, ViTForImageClassification
 
 class ViT:
     def __init__(self) -> None:
-        _processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-        _model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+        # As the Vision Transformer expects each image to be of the same size (resolution),
+        # one can use ViTImageProcessor to resize (or rescale) and normalize images for the model.
+        self._processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+        # Load from pretrained vit model
+        self._model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+        # Re-initialize weights for data valuation
+        self._model.init_weights()
+
+    def fit(self):
+        pass
