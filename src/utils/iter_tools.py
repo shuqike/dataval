f93c743d5830e452866c7155f36c88540c5d93a0
Griffith <shuqike.research@gmail.com>
2023-04-19 21:47:21 +0800

    Update iter_tools.py

--- src/utils/iter_tools.py
+++ src/utils/iter_tools.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def error(mem):
+def error(history):
     """Evaluate the changes of the most recent 100 updates
     """
-    if len(mem) < 100:
+    if len(history) < 100:
         return 1.0
-    all_vals = (np.cumsum(mem, 0)/np.reshape(np.arange(1, len(mem)+1), (-1,1)))[-100:]
+    all_vals = (np.cumsum(history, 0)/np.reshape(np.arange(1, len(history)+1), (-1,1)))[-100:]
     errors = np.mean(np.abs(all_vals[-100:] - all_vals[-1:])/(np.abs(all_vals[-1:]) + 1e-12), -1)
     return np.max(errors)
