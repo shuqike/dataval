54c1366c63385237bc3e99377e861ea5de86175e
iquhsek <shuqike.research@gmail.com>
2023-05-04 20:48:18 +0800

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,3 +3,4 @@ from src.utils.iter_tools import error
 from src.utils.data_tools import create_dataset, download_openML_dataset, CustomDataloader, CustomDataset
 from src.utils.experiment_tools import noisy_detection_experiment
 from src.utils.experiment_tools import point_removal_experiment
+from src.utils.fast_kmeans import KMeans, MultiKMeans
