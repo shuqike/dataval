3ebe36681db2a8ad36c5514f8e6db3ce7adef6ea
Griffith <shuqike.research@gmail.com>
2023-05-06 18:24:39 +0800

    Go encoder

--- src/utils/experiment_tools.py
+++ src/utils/experiment_tools.py
@@ -2,6 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 import copy
 import numpy as np
+import keras
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.cluster import KMeans
 import torchvision
@@ -138,9 +139,38 @@ def create_noisy_mnist(method='uniform', noise_level='normal'):
             if predicted_labels[i] == y_train[i]:
                 predicted_labels[i] = (predicted_labels[i]+1)%10
 
+    # encode features
+    # This is the size of our encoded representations
+    encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+    # This is our input image
+    input_img = keras.Input(shape=(784,))
+    # "encoded" is the encoded representation of the input
+    encoded = keras.layers.Dense(encoding_dim, activation='relu')(input_img)
+    # "decoded" is the lossy reconstruction of the input
+    decoded = keras.layers.Dense(784, activation='sigmoid')(encoded)
+    # This model maps an input to its reconstruction
+    autoencoder = keras.Model(input_img, decoded)
+    # This model maps an input to its encoded representation
+    encoder = keras.Model(input_img, encoded)
+    # This is our encoded (32-dimensional) input
+    encoded_input = keras.Input(shape=(encoding_dim,))
+    # Retrieve the last layer of the autoencoder model
+    decoder_layer = autoencoder.layers[-1]
+    # Create the decoder model
+    decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+    autoencoder.fit(x_train, x_train,
+        epochs=50,
+        batch_size=256,
+        shuffle=True,
+        validation_data=(x_test, x_test)
+    )
+
     # replace training labels with noisy labels
     # then return data and noisy indices
-    return x_train, predicted_labels, x_test, y_test, noisy_idxs
+    return encoder.predict(x_train), predicted_labels, encoder.predict(x_test), y_test, noisy_idxs
 
 def online_noisy_detection_experiment(T=10, noise_level='normal'):
     """
