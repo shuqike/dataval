5f31f856e1bf3d95925c43d0d1f564ced81a5171
Griffith <shuqike.research@gmail.com>
2023-05-06 18:42:19 +0800

    Update experiment_tools.py

--- src/utils/experiment_tools.py
+++ src/utils/experiment_tools.py
@@ -1,6 +1,8 @@
 import warnings
 warnings.filterwarnings('ignore')
 import copy
+import os
+import sys
 import numpy as np
 import keras
 from sklearn.linear_model import LogisticRegression, LinearRegression
@@ -139,34 +141,41 @@ def create_noisy_mnist(method='uniform', noise_level='normal'):
             if predicted_labels[i] == y_train[i]:
                 predicted_labels[i] = (predicted_labels[i]+1)%10
 
+    else: raise NotImplementedError
+
     # encode features
-    # This is the size of our encoded representations
-    encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
-    # This is our input image
-    input_img = keras.Input(shape=(784,))
-    # "encoded" is the encoded representation of the input
-    encoded = keras.layers.Dense(encoding_dim, activation='relu')(input_img)
-    # "decoded" is the lossy reconstruction of the input
-    decoded = keras.layers.Dense(784, activation='sigmoid')(encoded)
-    # This model maps an input to its reconstruction
-    autoencoder = keras.Model(input_img, decoded)
-    # This model maps an input to its encoded representation
-    encoder = keras.Model(input_img, encoded)
-    # This is our encoded (32-dimensional) input
-    encoded_input = keras.Input(shape=(encoding_dim,))
-    # Retrieve the last layer of the autoencoder model
-    decoder_layer = autoencoder.layers[-1]
-    # Create the decoder model
-    decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
-    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-    autoencoder.fit(x_train, x_train,
-        epochs=50,
-        batch_size=256,
-        shuffle=True,
-        validation_data=(x_test, x_test)
-    )
+    try:
+        encoder = keras.models.load_model('../data/pretrained_models/mnist_encoder')
+    except:
+        # Refer to https://blog.keras.io/building-autoencoders-in-keras.html
+        # This is the size of our encoded representations
+        encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+        # This is our input image
+        input_img = keras.Input(shape=(784,))
+        # "encoded" is the encoded representation of the input
+        encoded = keras.layers.Dense(encoding_dim, activation='relu')(input_img)
+        # "decoded" is the lossy reconstruction of the input
+        decoded = keras.layers.Dense(784, activation='sigmoid')(encoded)
+        # This model maps an input to its reconstruction
+        autoencoder = keras.Model(input_img, decoded)
+        # This model maps an input to its encoded representation
+        encoder = keras.Model(input_img, encoded)
+        # This is our encoded (32-dimensional) input
+        encoded_input = keras.Input(shape=(encoding_dim,))
+        # Retrieve the last layer of the autoencoder model
+        decoder_layer = autoencoder.layers[-1]
+        # Create the decoder model
+        decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+        autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+        x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+        x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+        autoencoder.fit(x_train, x_train,
+            epochs=50,
+            batch_size=256,
+            shuffle=True,
+            validation_data=(x_test, x_test)
+        )
+        encoder.save('../data/pretrained_models/mnist_encoder')
 
     # replace training labels with noisy labels
     # then return data and noisy indices
